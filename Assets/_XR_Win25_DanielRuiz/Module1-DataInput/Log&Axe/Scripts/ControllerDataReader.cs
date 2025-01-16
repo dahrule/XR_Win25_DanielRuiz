e965ace1d785c2dcef234513c17d4facb6dbe0ba
Daniel Ruiz <dahrule@hotmail.com>
2025-01-16 12:05:15 -0700

    Reading Velocity from controller completed

---
+++ Assets/_XR_Win25_DanielRuiz/Module1-DataInput/Log&Axe/Scripts/ControllerDataReader.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// This script is in charge of reading data from the touch  controller
+/// </summary>
+public class ControllerDataReader : MonoBehaviour
+{
+    [SerializeField] InputActionProperty velocityProperty;
+
+  /// <summary>
+  /// This is a Property
+  /// </summary>
+    public Vector3 Velocity { get; private set; } = Vector3.zero;
+
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        Velocity = velocityProperty.action.ReadValue<Vector3>();
+        Debug.Log("Velocity: " + Velocity);
+    }
+}
