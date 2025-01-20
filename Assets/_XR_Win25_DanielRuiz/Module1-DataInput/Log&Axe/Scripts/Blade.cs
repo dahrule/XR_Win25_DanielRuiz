8d20f2029b8aec450ceac4285312dac94a40218b
Daniel Ruiz <dahrule@hotmail.com>
2025-01-20 10:42:08 -0700

    First pass for LumberJack Interaction

--- Assets/_XR_Win25_DanielRuiz/Module1-DataInput/Log&Axe/Scripts/Blade.cs
+++ Assets/_XR_Win25_DanielRuiz/Module1-DataInput/Log&Axe/Scripts/Blade.cs
@@ -1,16 +1,55 @@
+using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
+[RequireComponent(typeof(XRGrabInteractable))]
 public class Blade : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public XRGrabInteractable m_grabInteractable;
+    public ControllerDataReader m_controllerDataReader;
+    XRBaseInteractor m_interactor;
+
+
+    
+    void Awake()
     {
-        
+        m_grabInteractable=GetComponent<XRGrabInteractable>();  
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnEnable()
     {
-        
+        if (m_grabInteractable == null)
+            return;
+
+        m_grabInteractable.selectEntered.AddListener(OnSelectEnter);
+        m_grabInteractable.selectExited.AddListener(ResetControllerDataReader);
+    }
+
+    private void ResetControllerDataReader(SelectExitEventArgs arg0)
+    {
+        m_controllerDataReader = null;
+    }
+
+    private void OnSelectEnter(SelectEnterEventArgs arg)
+    {
+        //Set te interactor that is grabbing the axe
+        m_interactor = arg.interactorObject as XRBaseInteractor;
+
+        //Set the ControllerDataReader
+        m_controllerDataReader=m_interactor.gameObject.GetComponentInParent<ControllerDataReader>();
+
+    }
+
+    private void OnDisable()
+    {
+        if (m_grabInteractable == null)
+            return;
+
+        m_grabInteractable.selectEntered.RemoveListener(OnSelectEnter);
+        m_grabInteractable.selectExited.RemoveListener(ResetControllerDataReader);
     }
 }
--- Assets/_XR_Win25_DanielRuiz/Module1-DataInput/Log&Axe/Scripts/Log.cs
+++ Assets/_XR_Win25_DanielRuiz/Module1-DataInput/Log&Axe/Scripts/Log.cs
@@ -1,16 +1,54 @@
+using System;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
+[RequireComponent(typeof(Collider))]
 public class Log : MonoBehaviour
 {
+    [SerializeField] GameObject logOne;
+    [SerializeField] GameObject logTwo;
+
+    Collider m_collider = null;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        m_collider = GetComponent<Collider>();
+        m_collider.isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Blade blade = null;
+        if (other.CompareTag("Blade"))
+        {
+            blade = other.GetComponentInParent<Blade>();
+        }
+
+        if (blade==null) 
+            return; //Quit this fucntion earlier if not blade script is found;
+
+        if (blade.m_controllerDataReader == null)
+            return;
+
+        Split(); 
+
     }
+
+    private void Split()
+    {
+        EnablePhysics(logOne);
+        EnablePhysics(logTwo);
+
+    }
+
+    void EnablePhysics(GameObject log)
+    {
+        log.transform.parent = null;
+
+        Rigidbody rg = log.GetComponent<Rigidbody>();
+        rg.useGravity = true;
+        rg.isKinematic = false;
+    }
+        
 }
