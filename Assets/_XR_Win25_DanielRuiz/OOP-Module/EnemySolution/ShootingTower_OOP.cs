69ad0d5ccc1e807e363725f182917d5e498dd8f6
Daniel Ruiz <dahrule@hotmail.com>
2025-02-06 12:52:24 -0700

    OOP Exercise solved

--- Assets/_XR_Win25_DanielRuiz/OOP-Module/EnemySolution/ShootingTower_OOP.cs
+++ Assets/_XR_Win25_DanielRuiz/OOP-Module/EnemySolution/ShootingTower_OOP.cs
@@ -2,5 +2,35 @@ using UnityEngine;
 
 public class ShootingTower_OOP : Enemy
 {
-   
+    [SerializeField] private Projectile m_projectilePrefab;
+    [SerializeField] Transform m_torret;
+
+    protected override void Attack()
+    { 
+        base.Attack();
+        AimTorret(m_playerTarget);
+        Fire();
+    }
+    private void AimTorret(Transform target)
+    {
+        // Get the direction from the object to the player
+        Vector3 direction = m_playerTarget.position - transform.position;
+
+        // Zero out the Y component to prevent rotation on the vertical axis
+        direction.y = 0;
+
+        if (direction != Vector3.zero)
+        {
+            // Create a rotation towards the target while ignoring vertical rotation
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = targetRotation;
+        }
+    }
+
+    private void Fire()
+    {
+        Projectile projectile = Instantiate(m_projectilePrefab, m_torret.position, m_torret.rotation);
+        projectile.Shoot(m_attackDamage);
+    }
 }
+
--- Assets/_XR_Win25_DanielRuiz/OOP-Module/EnemySolution/Slime_OOP.cs
+++ Assets/_XR_Win25_DanielRuiz/OOP-Module/EnemySolution/Slime_OOP.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class Slime_OOP : Enemy
 {
-    
+    protected override void Update()
+    {
+        base.Update();
+
+        if(!IsWithinAttackRange)
+            MoveTowardsPlayer();
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        if (m_playerTarget == null) return;
+
+        transform.position = Vector3.MoveTowards(transform.position, 
+            m_playerTarget.position, m_moveSpeed * Time.deltaTime);
+
+    }
 }
